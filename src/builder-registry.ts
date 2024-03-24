75d9eeb057c86fb8b9fcebfcf515679e7e86d7c6
Robert <Robirth@users.noreply.github.com>
2024-03-24 11:30:13 -0400

    Add new feature

--- src/builder-registry.ts
+++ src/builder-registry.ts
@@ -4,6 +4,7 @@ import Slide from "./components/Slide/Slide";
 import BotPage from "./components/Bots/BotPage";
 import Counter from "./components/Counter/Counter";
 import ThreeScene from "./components/ThreeScene/ThreeScene";
+import Menu from "./components/Menu/Menu";
 
 Builder.registerComponent(Counter, {
   name: "Counter",
@@ -25,4 +26,7 @@ Builder.registerComponent(ThreeScene, {
 
 Builder.registerComponent(Slide, {
   name: "Slide",
+});
+Builder.registerComponent(Menu, {
+  name: "Menu",
 });
