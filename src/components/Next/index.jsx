f66baac0c898c035ef1613ccbab8a14650d69543
Robert <Robirth@users.noreply.github.com>
2024-03-29 10:08:48 -0400

    Add new feature

--- src/components/Next/index.jsx
+++ src/components/Next/index.jsx
@@ -3,31 +3,29 @@ import '.dist/base.css';
 import '.dist/base.css';
 
 
-  {/* Hello world */}
-return (
-  <React.Fragment>
-    
-  
-    <main>
-      <div className="frame">
-        <div className="frame__title-wrap">
-          <h1 className="frame__title">HUMAN|NOTHUMAN</h1>
-        </div>
-        <p className="frame__tip">Click &amp; drag</p>
-      </div>
-      <div className="content" id="app">
-        <div>
-          {/* the content */}
-        </div>
-      </div>
-    </main>
-    <div className="cursor">
-      <div className="cursor__inner cursor__inner--circle" />
-      <div className="cursor__inner cursor__inner--dot" />
-    </div>
-  </React.Fragment>
-);
 
+const Next = () => {
+  return (
+    <React.Fragment>
+      <main>
+        <div className="frame">
+          <div className="frame__title-wrap">
+            <h1 className="frame__title">HUMAN|NOTHUMAN</h1>
+          </div>
+          <p className="frame__tip">Click &amp; drag</p>
+        </div>
+        <div className="content" id="app">
+          <div>
+            {/* the content */}
+          </div>
+        </div>
+      </main>
+      <div className="cursor">
+        <div className="cursor__inner cursor__inner--circle" />
+        <div className="cursor__inner cursor__inner--dot" />
+      </div>
+    </React.Fragment>
+  );
+}
 
 export default Next;
-
