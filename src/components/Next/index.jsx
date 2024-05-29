a3d3ef07f8fd3345901d5ead38bae6bbfbaf0d7f
Robert <Robirth@users.noreply.github.com>
2024-05-29 09:52:19 -0400

    Add new feature

--- src/components/Next/index.jsx
+++ src/components/Next/index.jsx
@@ -1,7 +1,8 @@
 "use client";
 import React from 'react';
-import "./dist/base.5f386e0a.css";
-import "./dist/styles.485f1646.css";
+import "./base.jsx";
+import "./dist/base.98fd6c19.css";
+import "./dist/styles.b61e60ae.css";
 import "./dist/js.fcf4b9da.js";
 
 const Next = () => {
