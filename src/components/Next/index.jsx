f5062bde6b66385fd0e916127602194dc61d1a76
Robert <Robirth@users.noreply.github.com>
2024-05-31 13:17:27 -0400

    Add new feature

--- src/components/Next/index.jsx
+++ src/components/Next/index.jsx
@@ -1,32 +1,74 @@
-"use client";
-import React from 'react';
-import "./base.jsx";
+import { useEffect } from 'react';
 import "./dist/base.5f386e0a.css";
 import "./dist/styles.485f1646.css";
-import "./dist/js.771ef394.js";
 
-const Next = () => {
+
+
+
+export default function Home() {
+  useEffect(() => {
+    // Dynamically load the JavaScript file on the client-side
+    const script = document.createElement('script');
+    script.src = './dist/js.771ef394.js';
+    script.async = true;
+    document.body.appendChild(script);
+
+    // Clean up script when component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
   return (
-    <React.Fragment>
+    <>
+      <Next>
+        <title>HNH</title>
+        <meta name="description" content="A recreation of the sticky image effect seen on the websites of MakeReign and Ultranoir using three.js." />
+        <meta name="keywords" content="three.js, sticky, animation, webgl, javascript, scroll, drag" />
+        <meta name="author" content="Codrops" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://use.typekit.net/ncw8xrb.css" />
+        <link rel="stylesheet" href="/css/base.css" />
+        <link rel="stylesheet" href="/css/styles.css" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            document.documentElement.className = "js";
+            var supportsCssVars = function () {
+              var e,
+                t = document.createElement("style");
+              return (
+                (t.innerHTML = "root: { --tmp-var: bold; }"),
+                document.head.appendChild(t),
+                (e = !!(
+                  window.CSS &&
+                  window.CSS.supports &&
+                  window.CSS.supports("font-weight", "var(--tmp-var)")
+                )),
+                t.parentNode.removeChild(t),
+                e
+              );
+            };
+            supportsCssVars() ||
+              alert(
+                "Please view this demo in a modern browser that supports CSS Variables."
+              );
+          `,
+        }} />
+      </Next>
       <main>
         <div className="frame">
           <div className="frame__title-wrap">
             <h1 className="frame__title">HUMAN|NOTHUMAN</h1>
           </div>
-          <p className="frame__tip">Click &amp; drag</p>
         </div>
         <div className="content" id="app">
-          <div>
-            {/* the content */}
-          </div>
+          {/* the content */}
         </div>
       </main>
       <div className="cursor">
-        <div className="cursor__inner cursor__inner--circle" />
-        <div className="cursor__inner cursor__inner--dot" />
+        <div className="cursor__inner cursor__inner--circle"></div>
+        <div className="cursor__inner cursor__inner--dot"></div>
       </div>
-    </React.Fragment>
+    </>
   );
 }
-
-export default Next;
