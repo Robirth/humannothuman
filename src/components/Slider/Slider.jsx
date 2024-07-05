c1abae95cd3ca010dceef0333b053fdbabd321b2
Robert <Robirth@users.noreply.github.com>
2024-07-05 11:35:14 -0400

    daily update

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,11 +1,74 @@
-import React from "react";
-import "/img/app.js";
-import "/img/style.css";
-
+import React, { useEffect, useRef } from "react";
+import "./style.css";
 
 const Slider = () => {
+  const carouselRef = useRef(null);
+  const nextBtnRef = useRef(null);
+  const prevBtnRef = useRef(null);
+
+  useEffect(() => {
+    const carousel = carouselRef.current;
+    const nextBtn = nextBtnRef.current;
+    const prevBtn = prevBtnRef.current;
+    const items = carousel.querySelectorAll(".list .item");
+    const indicator = carousel.querySelector(".indicators");
+    const dots = indicator.querySelectorAll(".indicators ul li");
+
+    let active = 0;
+    const firstPosition = 0;
+    const lastPosition = items.length - 1;
+    let autoPlay;
+
+    const startAutoPlay = () => {
+      clearInterval(autoPlay);
+      autoPlay = setInterval(() => {
+        nextBtn.click();
+      }, 5000);
+    };
+    startAutoPlay();
+
+    const setSlider = () => {
+      const itemActiveOld = carousel.querySelector(".list .item.active");
+      if (itemActiveOld) itemActiveOld.classList.remove("active");
+      items[active].classList.add("active");
+
+      const dotActiveOld = indicator.querySelector(".indicators ul li.active");
+      if (dotActiveOld) dotActiveOld.classList.remove("active");
+      dots[active].classList.add("active");
+
+      indicator.querySelector(".number").innerText = "0" + (active + 1);
+      startAutoPlay();
+    };
+    setSlider();
+
+    nextBtn.onclick = () => {
+      active = active + 1 > lastPosition ? 0 : active + 1;
+      carousel.style.setProperty("--calculation", 1);
+      setSlider();
+    };
+
+    prevBtn.onclick = () => {
+      active = active - 1 < firstPosition ? lastPosition : active - 1;
+      carousel.style.setProperty("--calculation", -1);
+      setSlider();
+      clearInterval(autoPlay);
+      autoPlay = setInterval(() => {
+        nextBtn.click();
+      }, 5000);
+    };
+
+    dots.forEach((item, position) => {
+      item.onclick = () => {
+        active = position;
+        setSlider();
+      };
+    });
+
+    return () => clearInterval(autoPlay); // Cleanup interval on unmount
+  }, []);
+
   return (
-    <section className="carousel">
+    <section className="carousel" ref={carouselRef}>
       <div className="list">
         <div className="item">
           <figure>
@@ -66,10 +129,10 @@ const Slider = () => {
         </div>
       </div>
       <div className="arrows">
-        <button id="prev">
+        <button id="prev" ref={prevBtnRef}>
           <i className="fa-solid fa-chevron-left"></i>
         </button>
-        <button id="next">
+        <button id="next" ref={nextBtnRef}>
           <i className="fa-solid fa-chevron-right"></i>
         </button>
       </div>
