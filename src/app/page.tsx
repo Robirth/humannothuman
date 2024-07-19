e9772cade84521b1fb17818924e8ea0361593ed6
Robert <Robirth@users.noreply.github.com>
2024-07-19 13:30:28 -0400

    daily update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
-import Image from 'next/image'
 import * as React from "react";
 import Menu from '../components/Menu/Menu';
 
-
-
-
 export default function Home() {
   return (
-
-
     <div className="box-border flex relative flex-col shrink-0 w-full min-h-[20px] min-w-[20px]">
-
-
-
-      <a href="/test"> <img
-        loading="lazy"
-        srcSet="https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2Fcfdade1741a141b6a543d6f07cefe0a0%2F7b63df447f6d40519a6901550afe4a6d"
-        className="box-border object-cover overflow-hidden absolute shrink-0 w-full cursor-pointer pointer-events-auto aspect-[4.37] min-h-[20px] min-w-[20px] mt-[500px] z-[55]"
-      /></a>
+      <a href="/test" className="button">ENTER</a>
       <div className="fixed">
         {/* Desktop Video */}
         <video
@@ -48,6 +35,3 @@ export default function Home() {
     </div>
   );
 }
-
- 
-  
