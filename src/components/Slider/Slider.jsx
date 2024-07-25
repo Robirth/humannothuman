b1a6764200358bad7c31206196d173ccbb75d1bb
Robert <Robirth@users.noreply.github.com>
2024-07-25 13:47:55 -0400

    daily update

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -72,15 +72,13 @@ const Slider = () => {
       <div className="list">
         <div className="item">
           <figure>
-            <img src="img/3.png" alt="Item 3" />
+            <img src="img/1.png" alt="Item 3" />
           </figure>
           <div className="content">
-            <p className="category">Work</p>
-            <h2>Concentric</h2>
+           
+            <h2>ConcentricLife</h2>
             <p className="description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-              facere ipsa blanditiis quidem dignissimos enim quam corrupti
-              praesentium ipsam assumenda?
+            ConcentricLife is a health and wellness company that leverages technology to provide personalized healthcare solutions. It focuses on integrating data and advanced analytics to improve patient outcomes and support proactive health management.
             </p>
             <div className="more">
               <button>
@@ -89,17 +87,16 @@ const Slider = () => {
             </div>
           </div>
         </div>
+        
         <div className="item">
           <figure>
             <img src="img/2.png" alt="Item 2" />
           </figure>
           <div className="content">
-            <p className="category">Work</p>
+         
             <h2>New York Times</h2>
             <p className="description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-              facere ipsa blanditiis quidem dignissimos enim quam corrupti
-              praesentium ipsam assumenda?
+            The New York Times, established in 1851, is a globally recognized newspaper known for its comprehensive and in-depth reporting. With a commitment to journalistic excellence, it covers a wide range of topics including politics, international news, business, culture, and science.
             </p>
             <div className="more">
               <button>
@@ -110,15 +107,64 @@ const Slider = () => {
         </div>
         <div className="item">
           <figure>
-            <img src="img/1.png" alt="Item 1" />
+            <img src="img/3.png" alt="Item 2" />
           </figure>
           <div className="content">
-            <p className="category">Work</p>
+           
             <h2>Victoria's Secret</h2>
             <p className="description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-              facere ipsa blanditiis quidem dignissimos enim quam corrupti
-              praesentium ipsam assumenda?
+            Victoria's Secret is a renowned American retailer specializing in women's lingerie, sleepwear, and beauty products. 
+            </p>
+            <div className="more">
+              <button>
+                <i className="fa-solid fa-play"></i> See More
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <figure>
+            <img src="img/4.png" alt="Item 2" />
+          </figure>
+          <div className="content">
+           
+            <h2>Neuberger Berman</h2>
+            <p className="description">
+            Neuberger Berman is a private, independent, employee-owned investment management firm founded in 1939. It offers a wide range of investment solutions, including equity, fixed income, and alternative investments, catering to institutional and individual clients globally.
+            </p>
+            <div className="more">
+              <button>
+                <i className="fa-solid fa-play"></i> See More
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <figure>
+            <img src="img/5.png" alt="Item 2" />
+          </figure>
+          <div className="content">
+           
+            <h2>Merton D. Simpson Gallery</h2>
+            <p className="description">
+            The Merton D. Simpson Gallery, founded by the renowned artist and collector Merton D. Simpson, is celebrated for its extensive collection of African and African-American art.
+            </p>
+            <div className="more">
+              <button>
+                <i className="fa-solid fa-play"></i> See More
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="item">
+          <figure>
+            <img src="img/6.png" alt="Item 1" />
+          </figure>
+          <div className="content">
+           
+            <h2>Time Is Illmatic</h2>
+            <p className="description">
+            Time is Illmatic is a 2014 documentary film that delves into the creation and cultural impact of Nas' iconic debut album Illmatic.
             </p>
             <div className="more">
               <button>
