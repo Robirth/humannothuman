9a37dc62877e6595f6a9159fd4e0e230ca9cbb57
Robert <Robirth@users.noreply.github.com>
2024-11-06 08:30:05 -0500

    daily update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const withBuilderDevTools = require("@builder.io/dev-tools/next")();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBuilderDevTools({});
+const nextConfig = withBuilderDevTools({
+  images: {
+    domains: ['cdn.builder.io'],
+  },
+  // add other configurations here if needed
+});
 
 module.exports = nextConfig;
